b1303fb9907aa2ba0aabb6fae2b70faa23ae0265
Jariskan <sergio.marin.alvarez@gmail.com>
2025-07-14 11:28:19 +0200

    Update main.cpp to include memory address and value display for string variables

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 10:33:44 by sergio            #+#    #+#             */
-/*   Updated: 2025/07/14 10:35:31 by sergio           ###   ########.fr       */
+/*   Updated: 2025/07/14 11:27:39 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,18 @@
 
 int main(void)
 {
+    std::string brain = "HI THIS IS BRAIN";
+    std::string* stringPTR = &brain;
+    std::string& stringREF = brain;
+
+    std::cout << "Memory address of brain          : " << &brain << std::endl;
+    std::cout << "Memory address held by stringPTR : " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF : " << &stringREF << std::endl;
+    std::cout << std::endl;
+    std::cout << "Value of brain                   : " << brain << std::endl;
+    std::cout << "Value pointed to by stringPTR    : " << *stringPTR << std::endl;
+    std::cout << "Value referenced by stringREF    : " << stringREF << std::endl;
+    
     return (0);
 }
 
